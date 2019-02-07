3014f361083ad916d0d6ca8ca69a8dbe7ab38b1b
AaronCur <c00202505@itcarlow.ie>
2019-02-07 11:07:16 +0000

    updated

--- ARGO/splash.h
+++ ARGO/splash.h
@@ -1,1 +1,14 @@
 #pragma once
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+
+class SplashScreen
+{
+public:
+	SplashScreen();
+	~SplashScreen();
+
+	void update(double dt);
+	void render(SDL_Renderer * renderer);
+};
