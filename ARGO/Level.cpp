f2b60e3daa99b917d885555d74d60cd439652a22
AaronCur <c00202505@itcarlow.ie>
2019-02-21 10:10:03 +0000

    Added node loading

--- ARGO/Level.cpp
+++ ARGO/Level.cpp
@@ -119,6 +119,19 @@ void level::load(const std::string& path, SDL_Renderer* ren) {
 					MapObjects t(x, y, width, height);
 					m_teleport.push_back(t);
 				}
+				else if (object.getName() == "Node")
+				{
+					float x, y, width, height;
+					std::string type;
+					x = object.getPosition().x;
+					y = object.getPosition().y;
+					width = object.getAABB().width;
+					height = object.getAABB().height;
+					type = object.getType();
+
+					NodeObjects n(x, y, width, height, type);
+					m_nodes.push_back(n);
+				}
 			}
 			
 		}
