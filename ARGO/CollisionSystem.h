7dea651a83667bd0ade2756f7eb7761299b79b85
AaronCur <c00202505@itcarlow.ie>
2019-02-25 09:49:01 +0000

    Updated

--- ARGO/CollisionSystem.h
+++ ARGO/CollisionSystem.h
@@ -35,7 +35,7 @@ private:
 	CollisionComponent* collide;
 	ScoreComponent* score;
 	PickUpComponent* pickup;
-	AIComponent* ai;
+//	AIComponent* ai;
 	float x1, y1, x2, y2, width1, height1, width2, height2;
 	float x3, y3, x4, y4, width3, height3, width4, height4;
 	float time;
